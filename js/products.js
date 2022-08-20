5e871217d781a558f3f8d56fa5569476e53f9218
gambettasanti <santi.gambetta.586@gmail.com>
2022-08-20 20:09:44 -0300

    Json
    Json Terminado

--- js/products.js
+++ js/products.js
@@ -1,20 +1,51 @@
 const AUTOS_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+let currentProductArray = [];
+let minCount = undefined;
+let maxCount = undefined;
 
-const container = document.getElementById("container");
+function showProductList(){
 
-function showData(dataArray) {
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductArray.products.length; i++){
+        let product = currentProductArray.products[i];
 
-    for (const item of dataArray) {
-         container.innerHTML += `<p> ${item.name} <p>`;
+        if (((minCount == undefined) || (minCount != undefined && parseInt(product.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.productCount) <= maxCount))){
 
+            htmlContentToAppend += `
+            <div onclick="setCatID(${product.id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost} </h4>
+                            <small class="text-muted">${product.soldCount} vendidos</small>
+                        </div>
+                        <p class="mb-1">${product.description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
     }
-
 }
 
-fetch(AUTOS_URL)
-    .then(function (response) {
-        return resoponse.json();
-    })
-    .then(function (json) {
-        showData(json.catID);
-    })
+
+document.addEventListener("DOMContentLoaded", function(e){
+
+    getJSONData(AUTOS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentProductArray = resultObj.data
+            showProductList()
+            //sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+        }
+    });
+
+   
+});
+
+
