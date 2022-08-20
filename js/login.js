d165878c96fc5e8adccca4bdd651524cf19b35e7
gambettasanti <santi.gambetta.586@gmail.com>
2022-08-19 23:01:12 -0300

    Login 3.0
    Haciendo funcionar el Login

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function loguear() {  
+
+    let pw1 = document.getElementById("psw1");  
+    let email = document.getElementById("email");
+    
+    if((email.value != '') && (pw1.value != '')){
+        window.location.href = 'home.html';
+    } else {  
+        alert('Complete los campos vacios');  
+    }
+
+  }
